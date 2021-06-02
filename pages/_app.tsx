1a18d58d8f3a764c80c6e472ce6a8c4a9782d78f
CAMILA MUELLER-LUDWIG <camila.dmuller@gmail.com>
2021-06-02 17:49:56 +0200

    feat: add progress bar for routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,22 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import theme from '../lib/utils/theme';
+import dynamic from 'next/dynamic';
+import 'nprogress/nprogress.css';
+
+const TopProgressBar = dynamic(
+  () => {
+    return import('../components/TopProgressBar/TopProgressBar');
+  },
+  { ssr: false },
+);
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <TopProgressBar />
         <Component {...pageProps} />
       </ThemeProvider>
     </ApolloProvider>
